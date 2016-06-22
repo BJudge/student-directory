7e5cb9bce514ad3faae530f35dbb87fd7220edcb
BartJudge <BartJudge@gmail.com>
2016-06-22 05:20:30 +0100

    the methods chapter

--- directory.rb
+++ directory.rb
@@ -13,10 +13,22 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-# and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each {|student| puts student}
-# finally, we print the total
-puts "Overall, we have #{students.count} great students"
-#it’s important that print() doesn’t add new line characters
+def print_header
+  puts "The student of Villains Academy"
+  puts "----------"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
