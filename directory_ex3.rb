0f6b7cb593f7342b594d7b54f22782c7ee0173e1
BartJudge <BartJudge@gmail.com>
2016-06-23 20:20:19 +0100

    exercise 3

---
+++ directory_ex3.rb
@@ -0,0 +1,31 @@
+def print_header
+ puts 'The students of my cohort at Makers Academy'
+  puts '-------------'
+end
+def print(students)
+students.map do |student|
+  name = student[:name]
+  if name.length < 12
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+end
+def print_footer(names)
+ puts "Overall, we have #{names.count} great students"
+end
+def input_students
+ puts "Please enter the names of the students"
+ puts "To finish, just hit return twice"
+ students = []
+ name = gets.chomp
+ while !name.empty? do
+   students << {name: name, cohort: :november}
+   puts "Now we have #{students.count} students"
+   name = gets.chomp
+ end
+ students
+end
+students = input_students
+print_header
+print(students)
+print_footer(students)
